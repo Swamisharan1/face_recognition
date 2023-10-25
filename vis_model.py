bdc87e03896df86fa0ac27357860169df32157ef
Swamisharan1 <147104986+Swamisharan1@users.noreply.github.com>
2023-10-25 15:54:04 +0530

    Update vis_model.py

--- vis_model.py
+++ vis_model.py
@@ -93,98 +93,99 @@ if img_file_buffer is not None:
         extracted_face = img_array[y1:y2,x1:x2]
         return extracted_face
 
-detector = MTCNN()
-extracted_face = return_face(detector, img_array)
-if len(extracted_face) > 0:  # If a face was found
-    resized_face = cv2.resize(extracted_face, (200, 200))
-    x = image.img_to_array(resized_face)
-    x = x / 255.0
-    x = np.expand_dims(x,axis = 0)
-    val = model.predict(x)
+    detector = MTCNN()
+    extracted_face = return_face(detector, img_array)
+    if len(extracted_face) > 0:  # If a face was found
+        resized_face = cv2.resize(extracted_face, (200, 200))
+        x = image.img_to_array(resized_face)
+        x = x / 255.0
+        x = np.expand_dims(x,axis = 0)
+        val = model.predict(x)
 
 
-if val == 1:
-    st.write('swami')
-else:
-    st.write('not swami')
+    if val == 1:
+        st.write('swami')
+    else:
+        st.write('not swami')
     
-if val != 1:
-    from cvzone.HandTrackingModule import HandDetector 
-    import cv2
-    import os
+    if val != 1:
+        from cvzone.HandTrackingModule import HandDetector 
+        import cv2
+        import os
 
-    detector = HandDetector()
+        detector = HandDetector()
 
-    def name():
-        print('''Displaying information:
-                Name:- Swamisharan Kumawat''')
+        def name():
+            print('''Displaying information:
+                    Name:- Swamisharan Kumawat''')
 
-    def add():
-        print('''Displaying information:
-                Address:-Sant Tukaram Nagar''')
+        def add():
+            print('''Displaying information:
+                    Address:-Sant Tukaram Nagar''')
 
-    def clas():
-        print('''Displaying information:
-                Class:- M.Sc. Data Science''')
+        def clas():
+            print('''Displaying information:
+                    Class:- M.Sc. Data Science''')
 
-    def college():
-        print('''Displaying information:
-                College:- DPU ACS College''')
+        def college():
+            print('''Displaying information:
+                    College:- DPU ACS College''')
 
-    def project():
-        print('''Displaying information:
-                Project Name:- FaceGesture Identity Access''')
-    def project_sum():
-        print('''Displaying information:
-                Description: "FaceGesture Identity Access" is a straightforward and inclusive project that
-                integrates facial recognition and hand gestures to provide secure identity verification
-                and efficient access to personalized information. This name explicitly conveys the core features
-                of the project in a common and accessible manner, making it clear that it's all about identity
-                and gesture-based access.''')
+        def project():
+            print('''Displaying information:
+                    Project Name:- FaceGesture Identity Access''')
+        def project_sum():
+            print('''Displaying information:
+                    Description: "FaceGesture Identity Access" is a straightforward and inclusive project that
+                    integrates facial recognition and hand gestures to provide secure identity verification
+                    and efficient access to personalized information. This name explicitly conveys the core features
+                    of the project in a common and accessible manner, making it clear that it's all about identity
+                    and gesture-based access.''')
   
     
 
 
-    handphoto = detector.findHands(img_bgr, draw = False)
+        handphoto = detector.findHands(img_bgr, draw = False)
 
-    if handphoto:
-        a  = len(handphoto[0])
-        def left_right():
-            fsum_l = 0
-            fsum_r = 0
-            for i in range(a):
-                if handphoto[0][i]['type']=='Right':
-                    lmlist  = handphoto[0][i]
-                    fingerstatus_right = detector.fingersUp(lmlist)
-                    for i in range(len(fingerstatus_right)):
-                        fsum_r = fsum_r+fingerstatus_right[i]
-                    break
-            for i in range(a):
-                if handphoto[0][i]['type']=='Left':
-                    lmlist1 = handphoto[0][i]
-                    fingerstatus_left = detector.fingersUp(lmlist1)
-                    for i in range(len(fingerstatus_left)):
-                        fsum_l = fsum_l+fingerstatus_left[i]
-                    break
-            fsum = fsum_l+fsum_r
-            return fsum
+        if handphoto:
+            a  = len(handphoto[0])
+            def left_right():
+                fsum_l = 0
+                fsum_r = 0
+                for i in range(a):
+                    if handphoto[0][i]['type']=='Right':
+                        lmlist  = handphoto[0][i]
+                        fingerstatus_right = detector.fingersUp(lmlist)
+                        for i in range(len(fingerstatus_right)):
+                            fsum_r = fsum_r+fingerstatus_right[i]
+                        break
+                for i in range(a):
+                    if handphoto[0][i]['type']=='Left':
+                        lmlist1 = handphoto[0][i]
+                        fingerstatus_left = detector.fingersUp(lmlist1)
+                        for i in range(len(fingerstatus_left)):
+                            fsum_l = fsum_l+fingerstatus_left[i]
+                        break
+                fsum = fsum_l+fsum_r
+                return fsum
         
-        if left_right() == 0:
-            print('no finger is up')
-        elif left_right() == 1:
-            name()
-        elif left_right() == 2:
-            add()
-        elif left_right() == 3:
-            clas()
-        elif left_right() == 4:
-            college()
-        elif left_right() == 5:
-            project()
-        elif left_right() ==6:
-            project_sum()
-        else:
-            print('Gesture can not be recognized')
+            if left_right() == 0:
+                print('no finger is up')
+            elif left_right() == 1:
+                name()
+            elif left_right() == 2:
+                add()
+            elif left_right() == 3:
+                clas()
+            elif left_right() == 4:
+                college()
+            elif left_right() == 5:
+                project()
+            elif left_right() ==6:
+                project_sum()
+            else:
+                print('Gesture can not be recognized')
             
     
+
 
