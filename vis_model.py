0e420a42c20d2c4ee60b1e6ba714fd313ba101c7
Swamisharan1 <147104986+Swamisharan1@users.noreply.github.com>
2023-10-25 19:39:18 +0530

    Update vis_model.py

--- vis_model.py
+++ vis_model.py
@@ -39,7 +39,7 @@ with zipfile.ZipFile('trained_model.zip', 'r') as zip_ref:
     zip_ref.extractall('.')
 
 # Now you can load your model
-model = load_model('trained_model/trained_model.h5')  # replace 'model/trained_model.h5' with the actual path of your .h5 file in the extracted folder
+model = load_model('trained_model.h5')  # replace 'model/trained_model.h5' with the actual path of your .h5 file in the extracted folder
 
 # Display the camera input widget
 img_file_buffer = st.camera_input("Take a picture")
